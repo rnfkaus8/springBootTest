cbde375191cef74a4377d2f07501fd55be1c64a7
rnfkaus8 <kth4350@gmail.com>
2021-09-05 16:14:07 +0900

    게시판 작성

--- src/main/java/com/taehyun/webservice/service/PostsService.java
+++ src/main/java/com/taehyun/webservice/service/PostsService.java
@@ -1,6 +1,10 @@
 package com.taehyun.webservice.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.taehyun.webservice.domain.posts.PostsRepository;
+import com.taehyun.webservice.dto.posts.PostsMainResponseDto;
 import com.taehyun.webservice.dto.posts.PostsSaveRequestDto;
 
 import org.springframework.stereotype.Service;
@@ -16,5 +20,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
